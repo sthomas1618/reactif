ee00c6920c25df4d2e2dc23b0d11bccba1490e68
Stephen Thomas <sthomas1618@gmail.com>
2012-12-15 14:47:46 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reactif::Application.config.secret_token = '35f7dcdfe93d1559e7b0fe785a5cc25037ecc375e1f02198dcbbd507c1aaa2a1667decbced86821dd0866d7b633c0b454002a69018802aa1013d3e76e364590a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Reactif::Application.config.session_store :cookie_store, key: '_reactif_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Reactif::Application.config.session_store :active_record_store
