1e244270aa1525d06e6cc523df6ba6d318b857ae
Stephen Thomas <sthomas1618@gmail.com>
2012-12-18 16:05:51 -0800

    Add Popular and New feeds

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
 
   def home
+    @popular_reactions = Reaction.find(:all, :limit => 5, :order=> 'views desc')
     @new_reactions = Reaction.find(:all, :limit => 5, :order=> 'created_at desc')
   end
 end
